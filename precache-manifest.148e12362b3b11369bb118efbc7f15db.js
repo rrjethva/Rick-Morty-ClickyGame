72efde94096425e49a613b55fed709756c31bbf5
Ravi Jethva <rrjethva@gmail.com>
2020-01-14 19:03:47 -0500

    Updates

---
+++ precache-manifest.148e12362b3b11369bb118efbc7f15db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889300dfdbe7ea2a2ad2ebdcaeb4a849",
+    "url": "/Rick-Morty-ClickyGame/index.html"
+  },
+  {
+    "revision": "4cc2f79bbd6fe4baa04e",
+    "url": "/Rick-Morty-ClickyGame/static/css/main.00751311.chunk.css"
+  },
+  {
+    "revision": "5be2ec15f59c91032832",
+    "url": "/Rick-Morty-ClickyGame/static/js/2.2bd258c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Rick-Morty-ClickyGame/static/js/2.2bd258c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cc2f79bbd6fe4baa04e",
+    "url": "/Rick-Morty-ClickyGame/static/js/main.7df1c131.chunk.js"
+  },
+  {
+    "revision": "67c707bdd7bb7b442728",
+    "url": "/Rick-Morty-ClickyGame/static/js/runtime-main.178abc72.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/Rick-Morty-ClickyGame/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/Rick-Morty-ClickyGame/static/media/bg.9a30e6c6.png"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/Rick-Morty-ClickyGame/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/Rick-Morty-ClickyGame/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/Rick-Morty-ClickyGame/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/Rick-Morty-ClickyGame/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/Rick-Morty-ClickyGame/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/Rick-Morty-ClickyGame/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/Rick-Morty-ClickyGame/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/Rick-Morty-ClickyGame/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/Rick-Morty-ClickyGame/static/media/mr.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/Rick-Morty-ClickyGame/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/Rick-Morty-ClickyGame/static/media/summer.afdbb7bf.png"
+  }
+]);
